0f3aa3812ab35ab0fa101f0fc40fbc74ff319005
FaletehanAlF <faletehanalfarabi09>
2026-09-01 08:13:29 +0700

    update project

--- lib/pages/homePage.dart
+++ lib/pages/homePage.dart
@@ -18,6 +18,7 @@ class _HomepageState extends State<Homepage> {
   Widget build(BuildContext context) {
     final data =
         ModalRoute.of(context)!.settings.arguments as Map<String, dynamic>;
+
     final nama = data["nama"];
     final umur = data["umur"];
 
@@ -27,62 +28,99 @@ class _HomepageState extends State<Homepage> {
         child: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
-            children: [Text("Nama Saya $nama"), Text("Umur Saya $umur")],
+            children: [
+              Image.asset(
+                "assets/images/kimmy.jpg",
+                width: 200,
+                height: 200,
+                fit: BoxFit.cover,
+              ),
+
+              const SizedBox(height: 20),
+
+              Text("Nama Saya $nama"),
+
+              Text("Umur Saya $umur"),
+            ],
           ),
         ),
       ),
+
       Profilepage(),
+
       Notifikasipage(),
+
       Keranjangpage(),
+
       Pesananpage(),
     ];
 
     return Scaffold(
-      appBar: AppBar(title: Text("Home Page")),
+      appBar: AppBar(
+        title: Text("Home Page"),
+      ),
+
       body: _pages[_currentIndex],
+
       bottomNavigationBar: BottomNavigationBar(
         type: BottomNavigationBarType.fixed,
         currentIndex: _currentIndex,
         selectedItemColor: Colors.orange,
         unselectedItemColor: Colors.grey,
+
         onTap: (index) {
           setState(() {
             _currentIndex = index;
           });
         },
+
         items: [
           BottomNavigationBarItem(
             icon: Icon(
               Icons.home,
-              color: _currentIndex == 0 ? Colors.orange : Colors.grey,
+              color: _currentIndex == 0
+                  ? Colors.orange
+                  : Colors.grey,
             ),
             label: "Home",
           ),
+
           BottomNavigationBarItem(
             icon: Icon(
               Icons.person,
-              color: _currentIndex == 1 ? Colors.purple : Colors.grey,
+              color: _currentIndex == 1
+                  ? Colors.purple
+                  : Colors.grey,
             ),
             label: "Profile",
           ),
+
           BottomNavigationBarItem(
             icon: Icon(
               Icons.notifications,
-              color: _currentIndex == 2 ? Colors.red : Colors.grey,
+              color: _currentIndex == 2
+                  ? Colors.red
+                  : Colors.grey,
             ),
             label: "Notifikasi",
           ),
+
           BottomNavigationBarItem(
             icon: Icon(
               Icons.shopping_cart,
-              color: _currentIndex == 3 ? Colors.green : Colors.grey,
+              color: _currentIndex == 3
+                  ? Colors.green
+                  : Colors.grey,
             ),
             label: "Keranjang",
           ),
+
           BottomNavigationBarItem(
             icon: Icon(
               Icons.receipt_long,
-              color: _currentIndex == 4 ? Colors.teal : Colors.grey,
+              color: _currentIndex == 4
+                  ? Colors.teal
+                  : Colors.grey,
             ),
             label: "Pesanan",
           ),
